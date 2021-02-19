5f91d2778fc6e81abab70bcaab762386688a78d0
Dylan McCarthy <Dylan.McCarthy@dpm.com.au>
2021-02-19 21:32:40 +1100

    Basic functionality done

---
+++ Tracking.py
@@ -0,0 +1,22 @@
+from datetime import datetime, tzinfo
+from skyfield.api import load
+from skyfield.positionlib import Geocentric
+import pytz
+import numpy as np
+import json
+
+class Tracking:
+    def __init__(self) -> None:
+        self.stations_url = 'https://celestrak.com/NORAD/elements/starlink.txt'
+        self.satellites = load.tle_file(self.stations_url)
+    
+    def get_pos(self):
+        positions = []
+        for sat in self.satellites:
+            ts = load.timescale()
+            gpo = sat.at(ts.now())
+            velocity = gpo.velocity.km_per_s
+            position = [sat.name, gpo.position.km[0], gpo.position.km[1],gpo.position.km[2], velocity[0], velocity[1],velocity[2]]
+            
+            positions.append(position)
+        return json.dumps(positions)
--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from Tracking import Tracking
+
+app = Flask(__name__)
+api = Api(app)
+track = Tracking()
+class SatelliteQuery(Resource):
+    def get(self):
+        result = track.get_pos()
+        return result
+
+api.add_resource(SatelliteQuery, '/')
+
+if __name__ == '__main__':
+    app.run()
